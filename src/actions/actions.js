e17c41bc15c2942215ad4aa5608872435686e7dd
andrea maurino <andrea.maurino@gmail.com>
2018-01-25 00:25:54 +0000

    first commit

---
+++ src/actions/actions.js
@@ -0,0 +1,12 @@
+import Reflux from 'reflux';
+
+const QualityActions = Reflux.createActions([
+  /**
+   * define your actions as strings below, for example:
+   */
+  'toggleStatus',
+  'showKeyValues'
+]);
+
+export default QualityActions;
+export { QualityActions };
